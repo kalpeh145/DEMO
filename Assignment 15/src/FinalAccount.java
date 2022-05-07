b34e9c5a172fc8fb6d0e062f19f9aa1e9fbc3d6b
DELL <DELL@DESKTOP-4L87TSB>
2022-05-07 15:28:56 +0530

    kk

---
+++ Assignment 15/src/FinalAccount.java
@@ -0,0 +1,6 @@
+public class FinalAccount {
+	public static void main(String[] args) {
+		AccountDetails mark=new AccountDetails();
+		mark.getAccountDetails();
+}
+}
---
+++ Assignment 15/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+public static void main(String[] args) {
+	Policy lic=new Policy();
+	System.out.println(lic.getPolicyDetails());
+}
+}
